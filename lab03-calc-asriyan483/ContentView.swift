2a8dcfcc26a084b317636a139e5a914ca2cf9578
BeklemishevG <111ag@rambler.ru>
2022-02-22 22:30:00 +0300

    Initial Commit

---
+++ lab03-calc-asriyan483/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lab03-calc-asriyan483
+//
+//  Created by user on 22.02.2022.
+//  Copyright © 2022 Asriyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
